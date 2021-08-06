e3d7cb00218d9db44d5d5e4293020fb8c9d0b374
technofoible <techno.foible@gmail.com>
2021-08-06 15:29:17 -0500

    Make new file to run game from.

---
+++ lib/tictactoe_main.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/tictactoe_game'
+require_relative '../lib/tictactoe_board'
+require_relative '../lib/tictactoe_player'
+require_relative '../lib/Array'
+
+game = TicTacToe_Game.new
+
+game.play_game
